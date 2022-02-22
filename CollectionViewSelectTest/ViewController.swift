9217a406227d2cd35a1127b0625e37a6dc6d076a
Yoonjiwon <k112751@koreainvestment.com>
2022-02-22 12:08:19 +0900

    add default layout

--- CollectionViewSelectTest/ViewController.swift
+++ CollectionViewSelectTest/ViewController.swift
@@ -8,12 +8,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 5
+        let menuView = UIView()
+//        menuView.translatesAutoresizingMaskIntoConstraints = false
+        menuView.heightAnchor.constraint(equalToConstant: 500).isActive = true
+        menuView.backgroundColor = .red
+        let stockView = UIView()
+//        stockView.translatesAutoresizingMaskIntoConstraints = false
+        stockView.heightAnchor.constraint(equalToConstant: 500).isActive = true
+        stockView.backgroundColor = .orange
+        
+        [menuView, stockView]
+            .forEach {
+                stackView.addArrangedSubview($0)
+            }
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        addSubviews()
+        setLayoutConstraint()
+        
     }
 
 
+}
+
+private extension ViewController {
+    func addSubviews() {
+        [scrollView]
+            .forEach {
+                view.addSubview($0)
+            }
+        scrollView.addSubview(contentView)
+        contentView.addSubview(stackView)
+    }
+    
+    func setLayoutConstraint() {
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        
+        stackView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+    }
 }
 
